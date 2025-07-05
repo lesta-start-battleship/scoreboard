a821298a9b4acda15ee77d32a51d8d1b74a6a042
Vinogradnij <vlud5@mail.ru>
2025-07-05 22:55:03 +0300

    feature(repositories): def get_user_by_foreign_id

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,4 +1,7 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import HTTPException, status
+
 from app.database.models.user import User
 
 
@@ -27,4 +30,27 @@ async def create_user(
     )
     session.add(user)
     await session.commit()
+    return user
+
+
+async def _get_user_by_foreign_id(
+    session: AsyncSession,
+    user_id: int,
+) -> User:
+    """
+    Получить Пользователя на основе id из внешнего сервиса
+
+    :param session: Сессия базы данных
+    :param user_id: id пользователя из внешней базы данных
+    :return: Пользователь, извлеченный из базы данных
+    """
+    exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found",
+    )
+    stmt = select(User).where(User.user_id == user_id)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+    if user is None:
+        raise exception
     return user
