94150ff88eca464c1f410027c64dc2522ba07e5a
Vinogradnij <vlud5@mail.ru>
2025-07-05 20:57:57 +0300

    edit(database): change DATABASE_URL type from URL to PostgresDsn

--- app/database/database.py
+++ app/database/database.py
@@ -3,22 +3,26 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from pydantic import PostgresDsn
 
 from app.config.preferences import DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT
 from app.database.models.base import Base
 
-DATABASE_URL = URL.create(
-    drivername="postgresql+asyncpg",
-    username=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME,
-    host=DB_HOST,
-    port=DB_PORT
-)
 
-engine = create_async_engine(url=DATABASE_URL, echo=True)
+def get_db_name() -> str:
+    DATABASE_URL = PostgresDsn(
+        f"postgresql+asyncpg://"
+        f"{DB_USER}:"
+        f"{DB_PASSWORD}@"
+        f"{DB_HOST}:"
+        f"{DB_PORT}/"
+        f"{DB_NAME}"
+    )
+    return str(DATABASE_URL)
+
+
+engine = create_async_engine(url=get_db_name(), echo=True)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
@@ -34,6 +38,6 @@ async def main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-if __name__ == '__main__':
-    asyncio.run(main())
 
+if __name__ == "__main__":
+    asyncio.run(main())
