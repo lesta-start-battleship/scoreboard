8835f55727b44ce41e73a899c3c6fd0bda568d0f
Vinogradnij <vlud5@mail.ru>
2025-07-06 09:17:34 +0300

    feature(repositories): def get_guild_by_foreign_id

--- app/repositories/guild.py
+++ app/repositories/guild.py
@@ -1,4 +1,6 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import HTTPException, status
 
 from app.database.models.guild import Guild
 
@@ -24,4 +26,27 @@ async def create_guild(
     session.add(guild)
     await session.commit()
     await session.refresh(guild)
+    return guild
+
+
+async def get_guild_by_foreign_id(
+    session: AsyncSession,
+    guild_id: int,
+) -> Guild:
+    """
+    Получить гильдию на основе id из внешнего сервиса
+
+    :param session: Сессия базы данных
+    :param guild_id: id гильдии из внешней базы данных
+    :return: Объект Гильдия, извлеченный из базы данных
+    """
+    exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Guild not found",
+    )
+    stmt = select(Guild).where(Guild.guild_id == guild_id)
+    result = await session.execute(stmt)
+    guild = result.scalar_one_or_none()
+    if guild is None:
+        raise exception
     return guild
