8674712fafb9f78d0cda1f1f33bbbbcebb1fa2db
Vinogradnij <Vlud5@mail.ru>
2025-07-09 02:04:43 +0300

    delete(database): def main

--- shared/database/database.py
+++ shared/database/database.py
@@ -1,11 +1,8 @@
-import asyncio
-
 from pydantic import PostgresDsn
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from shared.config.preferences import DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT
-from shared.database.models.base import Base
 
 
 def get_db_url():
@@ -25,12 +22,3 @@ DATABASE_URL = get_db_url()
 engine = create_async_engine(url=DATABASE_URL, echo=True)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def main():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
