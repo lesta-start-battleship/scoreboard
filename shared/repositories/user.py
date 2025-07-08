414f20d8456c2ea3f8c7cd1a2a7cb6871a948a26
Vinogradnij <Vlud5@mail.ru>
2025-07-08 16:22:39 +0300

    edit(repositories): возможность добавлять и удалять гильдию пользователю в update_user

--- shared/repositories/user.py
+++ shared/repositories/user.py
@@ -44,6 +44,8 @@ async def update_user(
     experience: int | None = None,
     rating: int | None = None,
     containers: int | None = None,
+    guild_id: int | None = None,
+    leaving_guild: bool = False,
 ) -> User:
     """
     Обновить данные пользователя. Возможно обновление одного и более параметров.
@@ -56,6 +58,8 @@ async def update_user(
     :param experience: Количество добавленного опыта
     :param rating: Количество добавленного рейтинга
     :param containers: Количество добавленных контейнеров
+    :param guild_id: id гильдии, которую необходимо добавить пользователю
+    :param leaving_guild: Флаг, сообщающий о том, что пользователю необходимо удалить гильдию
     :return: Пользователь с обновленными данными
     """
     user = await get_user_by_foreign_id(session=session, user_id=user_id)
@@ -69,6 +73,10 @@ async def update_user(
         user.rating += rating
     if containers:
         user.containers += containers
+    if guild_id:
+        user.guild_id = guild_id
+    if leaving_guild:
+        user.guild_id = None
     await session.commit()
     await session.refresh(user)
     return user
